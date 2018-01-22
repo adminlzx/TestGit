a3ef4fb5476e23b2e041b9d4e0bada9719e9e52b
adminlzx <460416801@qq.com>
2018-01-22 14:15:09 +0800

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("感觉真的不是很好用锕!");
+        for (int i = 0; i < 100; i++) {
+
+
+        }
     }
 }
